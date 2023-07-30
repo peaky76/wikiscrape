af03d17337cb1f590ee1b07aa2f7cedfa66fdd81
peaky76 <robertjamespeacock@gmail.com>
2023-07-30 22:19:40 +0100

    feat: Wikitable.data property

--- tests/test_wikitable.py
+++ tests/test_wikitable.py
@@ -6,3 +6,14 @@ def test_wikitable_headers():
     html = "<table><tr><th>Header 1</th><th>Header 2</th></tr></table>"
     table = BeautifulSoup(html, "html.parser").table
     assert Wikitable(table).headers == ["Header 1", "Header 2"]
+
+
+def test_wikitable_data():
+    html = """
+        <table>
+            <tr><td>Data A1</td><td>Data A2</td></tr>
+            <tr><td>Data B1</td><td>Data B2</td></tr>
+        </table>
+    """
+    table = BeautifulSoup(html, "html.parser").table
+    assert Wikitable(table).data == [["Data A1", "Data A2"], ["Data B1", "Data B2"]]
