64feefd65804673b9f684ebb809d0285eccbcd3d
peaky76 <robertjamespeacock@gmail.com>
2023-07-30 22:19:40 +0100

    feat: Wikitable.data property

--- wikiscrape/wikitable.py
+++ wikiscrape/wikitable.py
@@ -8,3 +8,10 @@ class Wikitable:
     @property
     def headers(self):
         return [th.text.strip() for th in self.table.find_all("th")]
+
+    @property
+    def data(self):
+        return [
+            [td.text.strip() for td in tr.find_all("td")]
+            for tr in self.table.find_all("tr")
+        ]
