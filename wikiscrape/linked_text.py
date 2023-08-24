a549071d076b76695d0225f136f8dc52c3ba87a2
peaky76 <robertjamespeacock@gmail.com>
2023-08-24 23:51:42 +0100

    feat: LinkedText.__repr__

--- wikiscrape/linked_text.py
+++ wikiscrape/linked_text.py
@@ -2,6 +2,9 @@ class LinkedText:
     def __init__(self, content):
         self.content = content
 
+    def __repr__(self):
+        return f"<LinkedText: {self.text} ({self.link})>"
+
     @property
     def link(self):
         return (self.content.a or {}).get("href")
