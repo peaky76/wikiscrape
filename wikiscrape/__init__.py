867182b9e818b19cd5e5fa91f93b7215a1aa5e84
peaky76 <robertjamespeacock@gmail.com>
2023-08-26 23:10:15 +0100

    feat: create Infobox class

--- wikiscrape/__init__.py
+++ wikiscrape/__init__.py
@@ -1,6 +1,7 @@
+from .infobox import Infobox
 from .linked_text import LinkedText
 from .markdown import Markdown
 from .wikipage import Wikipage
 from .wikitable import Wikitable
 
-__all__ = ["LinkedText", "Markdown", "Wikipage", "Wikitable"]
+__all__ = ["Infobox", "LinkedText", "Markdown", "Wikipage", "Wikitable"]
