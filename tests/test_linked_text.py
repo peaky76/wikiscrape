17fd16d556cbd20266e95a9badc13c35503c22a4
peaky76 <robertjamespeacock@gmail.com>
2023-09-03 18:58:57 +0100

    test: for bold LinkedText

--- tests/test_linked_text.py
+++ tests/test_linked_text.py
@@ -4,12 +4,19 @@ from wikiscrape import LinkedText
 LINK_AND_TEXT = BeautifulSoup(
     "<a href='https://www.alink.com'>Foobar</a>", "html.parser"
 )
+BOLD_LINK_AND_TEXT = BeautifulSoup(
+    "<b><a href='https://www.alink.com'>Foobar</a></b>", "html.parser"
+)
 TEXT_ONLY = BeautifulSoup("Foobar", "html.parser")
 NAVIGABLE_STRING = BeautifulSoup("Foobar", "html.parser").string
 
 
 def test_linked_text_init_with_link():
     assert LinkedText(LINK_AND_TEXT)
+
+
+def test_linked_text_init_with_bold_link():
+    assert LinkedText(BOLD_LINK_AND_TEXT)
 
 
 def test_linked_text_init_without_link():
@@ -25,6 +32,11 @@ def test_linked_text_repr_with_link():
     assert repr(LinkedText(LINK_AND_TEXT)) == expected
 
 
+def test_linked_text_repr_with_link():
+    expected = "<LinkedText: Foobar (https://www.alink.com)>"
+    assert repr(LinkedText(BOLD_LINK_AND_TEXT)) == expected
+
+
 def test_linked_text_repr_without_link():
     assert repr(LinkedText(TEXT_ONLY)) == "<LinkedText: Foobar (None)>"
 
@@ -37,6 +49,10 @@ def test_linked_text_link_with_link():
     assert LinkedText(LINK_AND_TEXT).link == "https://www.alink.com"
 
 
+def test_linked_text_link_with_bold_link():
+    assert LinkedText(BOLD_LINK_AND_TEXT).link == "https://www.alink.com"
+
+
 def test_linked_text_link_without_link():
     assert LinkedText(TEXT_ONLY).link is None
 
@@ -47,6 +63,10 @@ def test_linked_text_link_with_navigable_string():
 
 def test_linked_text_text_with_link():
     assert LinkedText(LINK_AND_TEXT).text == "Foobar"
+
+
+def test_linked_text_text_with_bold_link():
+    assert LinkedText(BOLD_LINK_AND_TEXT).text == "Foobar"
 
 
 def test_linked_text_text_without_link():
