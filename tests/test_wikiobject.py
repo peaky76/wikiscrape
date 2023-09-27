a0855a980cc43f2823975f6d02c39e42f4d435b3
peaky76 <robertjamespeacock@gmail.com>
2023-09-27 08:13:03 +0100

    refactor: create Wikiobject abstract class

---
+++ tests/test_wikiobject.py
@@ -0,0 +1,17 @@
+import pytest
+from bs4 import BeautifulSoup
+from wikiscrape import Wikiobject
+
+def test_wikiobject_init_raises_not_implemented_error():
+    with pytest.raises(NotImplementedError):
+        Wikiobject("foobar")
+
+
+def test_wikiobject_from_html_raises_not_implemented_error():
+    with pytest.raises(NotImplementedError):
+        Wikiobject.from_html("<div>Some content</div")
+
+        
+def test_wikiobject_from_soup_raises_not_implemented_error():
+    with pytest.raises(NotImplementedError):
+        Wikiobject.from_soup(BeautifulSoup("<div>Some content</div>", "html.parser"))
--- wikiscrape/__init__.py
+++ wikiscrape/__init__.py
@@ -2,7 +2,8 @@ from .coordinates import Coordinates
 from .infobox import Infobox
 from .linked_text import LinkedText
 from .markdown import Markdown
+from .wikiobject import Wikiobject
 from .wikipage import Wikipage
 from .wikitable import Wikitable
 
-__all__ = ["Coordinates", "Infobox", "LinkedText", "Markdown", "Wikipage", "Wikitable"]
+__all__ = ["Coordinates", "Infobox", "LinkedText", "Markdown", "Wikiobject", "Wikipage", "Wikitable"]
